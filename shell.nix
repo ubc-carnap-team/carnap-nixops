0d47e6d024ffb2dc9903bacf8c33b9fe051a34de
Jade Lovelace <software@lfcode.ca>
2022-02-19 20:51:40 -0800

    remove obsolete comment

--- shell.nix
+++ shell.nix
@@ -2,8 +2,6 @@ let sources = import ./nix/sources.nix;
     nixpkgs = import sources.nixpkgs { };
     #nixpkgs = import ../nixpkgs { };
     nixops = import sources.nixops;
-    # there's something broken with latest nixpkgs which we were accidentally using on here before
-    # TODO: diagnose it
     nixops-digitalocean = import sources.nixops-digitalocean { pkgs = nixpkgs; };
     inherit (nixpkgs) mkShell;
 in
